d0427ab3b90a4e5fa692f46c96a02554242a1f24
Pranav Maheshwari <pranav.maheshwari@toddleapp.com>
2023-10-05 13:33:41 +0530

    Publish/Subscribe feature

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import * as Ably from "ably/browser/static/ably-commonjs";
+import { useState } from "react";
 
-const USER_ID = "17740098669908281";
+const USER_ID = "User-1";
+let ablyClient;
+let channel;
 
 function App() {
-  let ablyClient;
+  const [connectionEstablished, setConnectionEstablished] = useState(false);
+  const [message, setMessage] = useState("");
+  const [subscribed, setSubscribed] = useState(false);
+  const [messages, setMessages] = useState([]);
+
   const printTokenDetails = () => {
     const tokenDetails = ablyClient.auth.tokenDetails;
 
@@ -38,20 +45,78 @@ function App() {
     console.log({ ablyClient });
     ablyClient.connection.once("connected", () => {
       console.log("connected");
+      setConnectionEstablished(true);
+      channel = ablyClient.channels.get("ably-auth-token-chat");
       printTokenDetails();
     });
 
     setInterval(printTokenDetails, 60000);
   };
 
+  const handleInputChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const subscribeChannel = () => {
+    console.log({ channel });
+    if (channel) {
+      setSubscribed(true);
+      channel.subscribe((message) => {
+        console.log("Received message: ", message);
+        setMessages((messages) => [...messages, message.data]);
+      });
+    }
+  };
+
+  const publishMessage = () => {
+    if (message && channel) {
+      const data = { user: USER_ID, message };
+      channel.publish("message", data, (err) => {
+        alert("Unable to publish message; err = " + err.message);
+        console.error("Unable to publish message; err = " + err.message);
+      });
+      setMessage("");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <button onClick={() => establishConnection()}>
-            Establish connection
-          </button>
-        </p>
+        <div>
+          {connectionEstablished ? (
+            <span>Connection established</span>
+          ) : (
+            <button onClick={() => establishConnection()}>
+              Establish connection
+            </button>
+          )}
+          <br />
+          <br />
+          {subscribed ? (
+            <span>Subscribed to the channel</span>
+          ) : (
+            <button onClick={() => subscribeChannel()}>
+              Subscribe to channel
+            </button>
+          )}
+          <br />
+          <br />
+          <input
+            type="text"
+            value={message}
+            onChange={handleInputChange}
+          />{" "}
+          &nbsp;
+          <button onClick={() => publishMessage()}>Publish Message</button>
+          <br />
+          <br />
+          {messages.map((message, index) => (
+            <div id={index}>
+              <h4 style={{ display: "inline" }}>{message.user}</h4> &rarr;{" "}
+              <span>{message.message}</span>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
