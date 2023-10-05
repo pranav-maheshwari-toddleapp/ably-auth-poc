d816a8246543cf6ea8b4005f13a20ce815fa2f86
Arnab Biswas <arnab@toddleapp.com>
2023-10-05 13:56:46 +0530

    Code Refactore: BE changes in code and packages

--- BE/server.js
+++ BE/server.js
@@ -1,26 +1,52 @@
-const Ably = require("ably");
-const cors = require("cors");
 const express = require("express");
 const app = express();
-
-const rest = new Ably.Rest({ key: "COLp9w.0CNo-Q:9wvmiPdAFHlTQKSO" });
-
+const Ably = require("ably");
+const cors = require("cors");
+const _ = require("lodash");
 app.use(cors());
 
 let count = 1;
 
-// Issue token requests to clients sending a request to the /auth endpoint
-app.get("/auth", async (req, res) => {
-  console.log(`API called ${count++}`, new Date().toLocaleString());
-  if (!req?.query?.clientId) {
-    res.status(500).send("Client ID not found!!!!");
+const getTokenParams = (jwtUser) => {
+  const { id, ut, roles, oid } = jwtUser;
+  let capabilities = {};
+  if (ut == "staff") {
+    capabilities = {
+      [`channelName:${oid}`]: ["publish", "subscribe", "presence"],
+    };
+  } else if (ut == "student") {
+    capabilities = {
+      [`channelName:${oid}`]: ["subscribe"],
+    };
+  } else if (ut == "parent") {
+    capabilities = {
+      [`channelName:${oid}`]: ["subscribe"],
+    };
   }
 
   const tokenParams = {
-    capability: { "*": ["publish", "subscribe"] },
-    clientId: req.query.clientId,
+    clientId: id,
     ttl: 60000,
+    capability: capabilities,
   };
+
+  return tokenParams;
+};
+
+app.get("/auth", async (req, res) => {
+  console.log(`API called ${count++}`, new Date().toLocaleString());
+
+  const { jwtUser } = req.query;
+  const { id: clientId = null } = jwtUser;
+
+  if (_.isNull(clientId)) {
+    res.status(500).send("Client ID not found!!!!");
+  }
+
+  const rest = new Ably.Rest({ key: "COLp9w.0CNo-Q:9wvmiPdAFHlTQKSO" });
+
+  const tokenParams = getTokenParams(jwtUser);
+  console.log("tokenParams:: ", tokenParams);
 
   await rest.auth.requestToken(tokenParams, (err, token) => {
     if (err) {
